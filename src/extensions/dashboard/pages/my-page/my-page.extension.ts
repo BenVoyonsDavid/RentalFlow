d9ce413b9762f7ba641d6eeeaaf018ce7cfed0bf
BenVoyonsDavid <93608059+BenVoyonsDavid@users.noreply.github.com>
2026-09-10 17:25:32 -0400

    Rename RentalFlow dashboard page

---
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,9 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: '4e66043b-5a38-4a75-a93f-cf1fa15d4ff7',
+  title: 'RentalFlow',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+  fullPage: false,
+});
