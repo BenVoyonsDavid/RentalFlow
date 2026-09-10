126b6c50820a3ba8d55f9ee33f44ac0bfb57354e
BenVoyonsDavid <93608059+BenVoyonsDavid@users.noreply.github.com>
2026-09-10 17:50:54 -0400

    Rename root dashboard page

--- src/extensions/dashboard/pages/my-page/my-page.extension.ts
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -2,7 +2,7 @@ import { extensions } from '@wix/astro/builders';
 
 export default extensions.dashboardPage({
   id: '4e66043b-5a38-4a75-a93f-cf1fa15d4ff7',
-  title: 'RentalFlow',
+  title: 'Tableau de bord',
   routePath: '',
   component: './extensions/dashboard/pages/my-page/my-page.tsx',
   fullPage: false,
