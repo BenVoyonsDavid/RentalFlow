5fe2ce2c89606b3dd7e252c47e76633187f0887b
BenVoyonsDavid <93608059+BenVoyonsDavid@users.noreply.github.com>
2026-09-10 17:25:25 -0400

    Register RentalFlow data collections

---
+++ src/extensions/backend/data-collections/data-collections.extension.ts
@@ -0,0 +1,8 @@
+import { extensions } from '@wix/astro/builders';
+import assetsCollection from './assets';
+
+export default extensions.dataCollections({
+  id: 'c1fe36e1-148a-4fed-91aa-ade5edec2a02',
+  name: 'Data Collections',
+  collections: [assetsCollection],
+});
