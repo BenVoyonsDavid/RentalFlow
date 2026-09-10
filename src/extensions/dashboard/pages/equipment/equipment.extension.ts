430a63e8ec449ec19c9b61971e6137ff9b2ed639
BenVoyonsDavid <93608059+BenVoyonsDavid@users.noreply.github.com>
2026-09-10 17:51:31 -0400

    Add Equipment dashboard page extension

---
+++ src/extensions/dashboard/pages/equipment/equipment.extension.ts
@@ -0,0 +1,9 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: '52d1588d-fc70-441a-aba5-dc65a48e07f4',
+  title: 'Équipements',
+  routePath: 'equipment',
+  component: './extensions/dashboard/pages/equipment/equipment.tsx',
+  fullPage: false,
+});
