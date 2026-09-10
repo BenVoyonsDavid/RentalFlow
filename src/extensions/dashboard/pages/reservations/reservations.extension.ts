cdc29816d7c3f1bfacdd06d3f6896ecab5e8136a
BenVoyonsDavid <93608059+BenVoyonsDavid@users.noreply.github.com>
2026-09-10 17:52:23 -0400

    Add Reservations dashboard page extension

---
+++ src/extensions/dashboard/pages/reservations/reservations.extension.ts
@@ -0,0 +1,9 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: 'c5635248-110a-4f29-aeee-a84bfaee3a91',
+  title: 'Calendrier / Réservations',
+  routePath: 'reservations',
+  component: './extensions/dashboard/pages/reservations/reservations.tsx',
+  fullPage: false,
+});
