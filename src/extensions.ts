a547948d1b8d368b7c815c84373851547783e44f
BenVoyonsDavid <93608059+BenVoyonsDavid@users.noreply.github.com>
2026-09-10 19:15:14 -0400

    Add dedicated RentalFlow calendar page

--- src/extensions.ts
+++ src/extensions.ts
@@ -2,14 +2,16 @@ import { app } from '@wix/astro/builders';
 import dashboardPage from './extensions/dashboard/pages/my-page/my-page.extension.ts';
 import equipmentPage from './extensions/dashboard/pages/equipment/equipment.extension.ts';
 import reservationsPage from './extensions/dashboard/pages/reservations/reservations.extension.ts';
+import calendarPage from './extensions/dashboard/pages/calendar/calendar.extension.ts';
 import customersPage from './extensions/dashboard/pages/customers/customers.extension.ts';
 import settingsPage from './extensions/dashboard/pages/settings/settings.extension.ts';
 import dataCollections from './extensions/backend/data-collections/data-collections.extension.ts';
 
 export default app()
   .use(dashboardPage)
-  .use(equipmentPage)
   .use(reservationsPage)
+  .use(calendarPage)
+  .use(equipmentPage)
   .use(customersPage)
   .use(settingsPage)
   .use(dataCollections);
