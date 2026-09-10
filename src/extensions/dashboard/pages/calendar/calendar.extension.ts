406b3dd2e410c782c194cb72573b94d077bc8641
BenVoyonsDavid <93608059+BenVoyonsDavid@users.noreply.github.com>
2026-09-10 19:14:37 -0400

    Add RentalFlow calendar dashboard page

---
+++ src/extensions/dashboard/pages/calendar/calendar.extension.ts
@@ -0,0 +1,9 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: 'b4a6d293-4fae-4fb7-a7d8-907e7d7f42b1',
+  title: 'Calendrier',
+  routePath: 'calendar',
+  component: './extensions/dashboard/pages/calendar/calendar.tsx',
+  fullPage: false,
+});
