c5a1e4396dedba520852c2d22c4c305bf07dff3e
BenVoyonsDavid <93608059+BenVoyonsDavid@users.noreply.github.com>
2026-09-10 18:20:28 -0400

    Register customers data collection

--- src/extensions/backend/data-collections/data-collections.extension.ts
+++ src/extensions/backend/data-collections/data-collections.extension.ts
@@ -2,9 +2,15 @@ import { extensions } from '@wix/astro/builders';
 import assetsCollection from './assets';
 import reservationsCollection from './reservations';
 import reservationItemsCollection from './reservation-items';
+import customersCollection from './customers';
 
 export default extensions.dataCollections({
   id: 'c1fe36e1-148a-4fed-91aa-ade5edec2a02',
   name: 'Data Collections',
-  collections: [assetsCollection, reservationsCollection, reservationItemsCollection],
+  collections: [
+    assetsCollection,
+    reservationsCollection,
+    reservationItemsCollection,
+    customersCollection,
+  ],
 });
