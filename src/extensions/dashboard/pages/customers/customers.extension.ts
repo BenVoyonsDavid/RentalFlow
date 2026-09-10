7205888519308f4eff4084012383651aab00537f
BenVoyonsDavid <93608059+BenVoyonsDavid@users.noreply.github.com>
2026-09-10 18:20:37 -0400

    Add Customers dashboard page

---
+++ src/extensions/dashboard/pages/customers/customers.extension.ts
@@ -0,0 +1,9 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: 'aded2636-c1a8-48aa-ae81-db5264196ce4',
+  title: 'Clients',
+  routePath: 'customers',
+  component: './extensions/dashboard/pages/customers/customers.tsx',
+  fullPage: false,
+});
