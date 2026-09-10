4e2b3c64fd047be8d2a093cde9147df0e0f90e71
BenVoyonsDavid <93608059+BenVoyonsDavid@users.noreply.github.com>
2026-09-10 17:52:47 -0400

    Add Settings dashboard page extension

---
+++ src/extensions/dashboard/pages/settings/settings.extension.ts
@@ -0,0 +1,9 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: 'da63679a-4145-4c0f-a391-5870f6809c36',
+  title: 'Paramètres',
+  routePath: 'settings',
+  component: './extensions/dashboard/pages/settings/settings.tsx',
+  fullPage: false,
+});
