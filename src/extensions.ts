cd4a642554abf3a25356336dd2a38bdd3958aa1a
BenVoyonsDavid <93608059+BenVoyonsDavid@users.noreply.github.com>
2026-09-10 18:21:31 -0400

    Register Customers dashboard page

--- src/extensions.ts
+++ src/extensions.ts
@@ -2,6 +2,7 @@ import { app } from '@wix/astro/builders';
 import dashboardPage from './extensions/dashboard/pages/my-page/my-page.extension.ts';
 import equipmentPage from './extensions/dashboard/pages/equipment/equipment.extension.ts';
 import reservationsPage from './extensions/dashboard/pages/reservations/reservations.extension.ts';
+import customersPage from './extensions/dashboard/pages/customers/customers.extension.ts';
 import settingsPage from './extensions/dashboard/pages/settings/settings.extension.ts';
 import dataCollections from './extensions/backend/data-collections/data-collections.extension.ts';
 
@@ -9,5 +10,6 @@ export default app()
   .use(dashboardPage)
   .use(equipmentPage)
   .use(reservationsPage)
+  .use(customersPage)
   .use(settingsPage)
   .use(dataCollections);
