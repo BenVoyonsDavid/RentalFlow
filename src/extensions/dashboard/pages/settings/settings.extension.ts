9d10612b931c261dcf3350594e74bcde5dcd0074
BenVoyonsDavid <93608059+BenVoyonsDavid@users.noreply.github.com>
2026-09-11 11:23:41 -0400

    feat(i18n): route settings through localized wrapper

--- src/extensions/dashboard/pages/settings/settings.extension.ts
+++ src/extensions/dashboard/pages/settings/settings.extension.ts
@@ -4,6 +4,6 @@ export default extensions.dashboardPage({
   id: 'da63679a-4145-4c0f-a391-5870f6809c36',
   title: 'Paramètres',
   routePath: 'settings',
-  component: './extensions/dashboard/pages/settings/settings-v2.tsx',
+  component: './extensions/dashboard/pages/settings/settings.localized.tsx',
   fullPage: false,
 });
