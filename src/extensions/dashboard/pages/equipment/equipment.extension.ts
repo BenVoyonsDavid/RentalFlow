665cc35a7003989a661af5fc9a7f83eb6e738850
BenVoyonsDavid <93608059+BenVoyonsDavid@users.noreply.github.com>
2026-09-11 11:23:26 -0400

    feat(i18n): route equipment through localized wrapper

--- src/extensions/dashboard/pages/equipment/equipment.extension.ts
+++ src/extensions/dashboard/pages/equipment/equipment.extension.ts
@@ -4,6 +4,6 @@ export default extensions.dashboardPage({
   id: '52d1588d-fc70-441a-aba5-dc65a48e07f4',
   title: 'Équipements',
   routePath: 'equipment',
-  component: './extensions/dashboard/pages/equipment/equipment.tsx',
+  component: './extensions/dashboard/pages/equipment/equipment.localized.tsx',
   fullPage: false,
 });
