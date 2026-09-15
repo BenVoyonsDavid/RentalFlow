315e3e7612e6d2920bfde53e688243c1e46d6dd4
BenVoyonsDavid <93608059+BenVoyonsDavid@users.noreply.github.com>
2026-09-15 08:30:43 -0400

    feat: localize reservation extras page

---
+++ src/extensions/dashboard/pages/reservation-extras/reservation-extras.localized.tsx
@@ -0,0 +1,4 @@
+import ReservationExtrasPage from './reservation-extras';
+import { withDashboardLocalization } from '../../../../intl/dashboard-page';
+
+export default withDashboardLocalization(ReservationExtrasPage);
